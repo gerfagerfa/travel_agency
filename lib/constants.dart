2117b853e3342601b9e6a78054bf87c91e5813b1
Germán Fajardo Sánchez <gerfagerfa@gmail.com>
2020-07-26 21:24:29 -0300

    finished

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,14 +2,3 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Color(0xFFFF8412);
 Color kBackgroundColor = Color(0xFFEFF4F8);
-
-LinearGradient kGradient = LinearGradient(
-  begin: Alignment.topCenter,
-  end: Alignment.bottomCenter,
-  stops: [0.2, 0.4, 0.7],
-  colors: [
-    Colors.black.withOpacity(0.3),
-    Colors.transparent,
-    Colors.black.withOpacity(0.3),
-  ],
-);
